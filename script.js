a8e8a6adc5279a53c48a82189cf22a88c38baf15
majid Amini <majidamini_s@yahoo.com>
2022-03-18 15:41:21 +0330

    axie project with Html and Css

---
+++ script.js
@@ -0,0 +1,41 @@
+const search_btn = document.querySelector('.glass');
+let input = document.querySelector('.search_input');
+let deleteBtn = document.querySelector('.delete');
+
+search_btn.addEventListener('click',function(){
+    input.classList.toggle('search_input--show');
+    deleteBtn.style.display = ('block');
+    if (!input.classList.contains('search_input--show')) {
+        deleteBtn.style.display = ('none');
+    }
+})
+
+deleteBtn.addEventListener('click',function(){
+    input.classList.remove('search_input--show');
+    deleteBtn.style.display = ('none');
+})
+
+
+// const light_btn = document.querySelector('.light');
+// light_btn.addEventListener('click',function(){
+//  document.body.classList.remove('dark_mode');
+//  document.body.classList.add('light_mode');
+ 
+   
+// })
+
+// const dark_btn = document.querySelector('.dark');
+// dark_btn.addEventListener('click',function(){
+//     document.body.classList.remove('light_mode');
+//     document.body.classList.add('dark_mode'); 
+// })
+
+
+
+let heartsIcons = document.querySelectorAll('.fa-heart');
+console.log(heartsIcons);
+// heartsIcons.forEach(item => event) {
+//     item.addEventListener('click',function(){
+        
+//     })
+// }
