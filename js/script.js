59468d37868a9d5d3683dbc9de92ee360eb1ae43
majid Amini <majidamini_s@yahoo.com>
2022-04-24 18:07:47 +0430

    edit profile page added

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ function scrollfunc() {
 
 //timer function
 let countDown = document.querySelector('#countdown');
-let countDownDate = new Date('april 12,2022 01:51:20').getTime();
+let countDownDate = new Date('april 13,2022 01:51:20').getTime();
 let interval = setInterval(function (){
 
     let now = new Date().getTime();
@@ -48,9 +48,15 @@ let interval = setInterval(function (){
     let hours = Math.floor ((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor ((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor ((distance % (1000 * 60)) / 1000);
-    console.log(seconds);
-
+    
     countDown.innerHTML = `${days}d : ${hours}h: ${minutes}m: ${seconds}s`;
 },1000)
+
+
+//tab info
+// const info = document.querySelectorAll('.info');
+// info.addEventListener('click',function(event){
+//     info.classList.toggle('blue_border');
+// })
 
 
