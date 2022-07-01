d8493819d86d4dfb1ca253266c702bda689f1dd8
majid Amini <majidamini_s@yahoo.com>
2022-07-01 19:35:23 +0430

    new input added to form with js when a user wants to register

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 'use strict'
 const signup = document.querySelector('#signup')
 let signInText = document.querySelector('#signIn_text')
+let submit = document.querySelector("#submit")
+let nameEl = document.querySelector(".name_total")
+console.log(nameEl);
 
 signup.addEventListener('click', changeElement)
 
@@ -8,10 +11,17 @@ function changeElement () {
 
     if (signInText.textContent == "Sign In") {
         signInText.textContent = "Sign Up"
-     signup.textContent = "Log In"
+        signup.textContent = "Log In"
+        submit.textContent = "Register"
+        nameEl.classList.add("display")
+        console.log(nameEl.classList);
+
+        
     } else {
         signInText.textContent = "Sign In"
         signup.textContent = "create an account"
+        submit.textContent = "Sign in"
+        nameEl.classList.remove("display")
     }
     
 }
